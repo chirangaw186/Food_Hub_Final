f5c5cdc7283e592e604936a8bf85fa0eee5279c7
CHARITH1995 <prasanna.charith32@gmail.com>
2019-01-25 15:57:51 +0530

    css styles

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './additemCSS.css';
+import { Link } from 'react-router-dom';
 import ImageUpload from './ImageUpload';
+import Links from './links'
 
 class AddItem extends Component {
 
@@ -168,8 +170,35 @@ class AddItem extends Component {
       });
 
   }
-
-
+  form() {
+    return (
+      <div className="idform" >
+        <form className="form-horizontal" onSubmit={this.onValueSubmit}>
+          <div className="form-group col-md-8">
+            <label htmlFor="exampleFormControlInput1"> Item ID :</label>
+            <input type="text" className="form-control" value={this.state.itemid} onChange={this.onIDValueChange} placeholder="Enter ID" />
+          </div>
+          <div className="form-group col-md-8">
+            <label htmlFor="exampleFormControlInput1"> Item Name :</label>
+            <input type="text" className="form-control" id="pwd" value={this.state.itemname} onChange={this.onNameValueChange} placeholder="Enter Name" />
+          </div>
+          <div className="form-group col-md-8">
+            <label htmlFor="exampleFormControlInput1"> Available Quantity :</label>
+            <input type="Number" className="form-control" value={this.state.qty} onChange={this.onQtyValueChange} id="email" placeholder="Enter Available Quantity" />
+          </div>
+          <div className="form-group col-md-8">
+            <label htmlFor="exampleFormControlInput1"> Price:</label>
+            <input type="Number" className="form-control" id="pwd" value={this.state.price} onChange={this.onPriceValueChange} placeholder="Enter Price" />
+          </div>
+          <div className="form-group">
+            <div className="col-sm-8">
+              <button className="btn btn-primary col-sm-2" id="subButton" onClick={this.handleChange}>Save</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
   render() {
 
     let { imagepreviewurl } = this.state;
@@ -196,67 +225,22 @@ class AddItem extends Component {
       </div>
       )
     }
-
-
     return (
-      <div className="container" >
-
-        <form className="form-horizontal" onSubmit={this.onValueSubmit}>
-          <h2>Add Item</h2>
-          <br />
-          <br />
-
-          <div className="form-group">
-            <label className="control-label col-sm-2">Item ID : </label>
-            <div className="col-sm-10">
-              <input type="text" className="form-control" value={this.state.itemid} onChange={this.onIDValueChange} placeholder="Enter ID" />
-            </div>
-          </div>
-          <div className="form-group">
-            <label className="control-label col-sm-2" >Item Name :</label>
-            <div className="col-sm-10">
-              <input type="text" className="form-control" id="pwd" value={this.state.itemname} onChange={this.onNameValueChange} placeholder="Enter Name" />
-            </div>
-          </div>
-          <div className="form-group">
-            <label className="control-label col-sm-2">Available Quantity :</label>
-            <div className="col-sm-10">
-              <input type="Number" className="form-control" value={this.state.qty} onChange={this.onQtyValueChange} id="email" placeholder="Enter Available Quantity" />
-            </div>
-          </div>
-          <div className="form-group">
-            <label className="control-label col-sm-2" >Price:</label>
-            <div className="col-sm-10">
-              <input type="Number" className="form-control" id="pwd" value={this.state.price} onChange={this.onPriceValueChange} placeholder="Enter Price" />
-            </div>
-          </div>
-
-
-
-          {/* <div className="form-group">
-    <div className="col-sm-offset-2 col-sm-10">
-      <button type="submit" className="btn btn-default"  >Submit</button>
-    </div>
-  </div> */}
-
-
-        </form>
-        <button className="btn btn-primary col-sm-2" id="subButton" onClick={this.handleChange}>Save</button>
-
-
-
-        <div className="container">
-          {$imageDIV}
+      <div className="container-fluid">
+        <h2 className="custitle">New Item</h2>
+        <div className="col-sm-2">
         </div>
-
-
-        {/* <div><img src={require('./upload/'+this.state.imagepath['imagepath'])} className="img-rounded img-responsive" alt="not available"/></div> */}
-
-
-
-
-
-
+        <div className="col-sm-8 contain">
+          <hr />
+          <div className="middle">
+            {this.form()}
+          </div>
+          <div className="form">
+            <div className="col-sm-8">
+              {$imageDIV}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/AddNewItem.js
+++ src/AddNewItem.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import './additemCSS.css';
-
+import { Link } from 'react-router-dom';
 import FileBase64 from 'react-file-base64';
+import Links from './links'
 
 class NewItem extends Component {
 
@@ -18,8 +19,8 @@ class NewItem extends Component {
             file: '',
             imagepreviewurl: '',
             uploadstatus: false,
-            files:null,
-            description:'Add Description',
+            files: null,
+            description: 'Add Description',
             shopid: this.props.match.params.id,
 
 
@@ -30,8 +31,8 @@ class NewItem extends Component {
         this.onQtyValueChange = this.onQtyValueChange.bind(this);
         this.onPriceValueChange = this.onPriceValueChange.bind(this);
         this.onDescriptionValueChange = this.onDescriptionValueChange.bind(this);
-      
-     
+
+
     }
 
 
@@ -70,11 +71,11 @@ class NewItem extends Component {
         });
     }
 
-   
-    getFiles(files){
+
+    getFiles(files) {
         this.setState({ files: files })
-      }
-    
+    }
+
 
 
 
@@ -82,22 +83,22 @@ class NewItem extends Component {
         //console.log(this.state.files.base64);
         // const data = new FormData();
         // data.append('file',this.state.selectedFile);
-            var myimage="alternate.jpg";
-        if(this.state.files==null){
-           console.log("inside if")
-            
-        }else{
-           myimage=this.state.files.base64
+        var myimage = "alternate.jpg";
+        if (this.state.files == null) {
+            console.log("inside if")
+
+        } else {
+            myimage = this.state.files.base64
         }
 
         const data = new FormData();
-        data.append('myfile',myimage);
-        data.append('itemid',this.state.itemid);
-        data.append('itemname',this.state.itemname);
-        data.append('qty',this.state.qty);
-        data.append('price',this.state.price);
-        data.append('description',this.state.description);
-        data.append('shopid',this.state.shopid);
+        data.append('myfile', myimage);
+        data.append('itemid', this.state.itemid);
+        data.append('itemname', this.state.itemname);
+        data.append('qty', this.state.qty);
+        data.append('price', this.state.price);
+        data.append('description', this.state.description);
+        data.append('shopid', this.state.shopid);
         // const item = {
         //     itemid: this.state.itemid,
         //     itemname: this.state.itemname,
@@ -108,9 +109,9 @@ class NewItem extends Component {
 
         fetch('http://localhost:4000/index/newfood', {
             method: "POST",
-           
-           
-            body:data   
+
+
+            body: data
         })
             .then(function (response) {
 
@@ -122,85 +123,61 @@ class NewItem extends Component {
 
     }
 
-
-    render() {
-
-  
-
-
-
-
-
+    form() {
         return (
-            <div className="container" >
-
+            <div className="idform" >
                 <form className="form-horizontal" onSubmit={this.onValueSubmit}>
-                    <h2>Add Item</h2>
-                    <br />
-                    <br />
-
-                    <div className="form-group">
-                        <label className="control-label col-sm-2">Item ID : </label>
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" value={this.state.itemid} onChange={this.onIDValueChange} placeholder="Enter ID" />
-                        </div>
+                    <div className="form-group col-md-8">
+                        <label htmlFor="exampleFormControlInput1"> Item ID :</label>
+                        <input type="text" className="form-control" value={this.state.itemid} onChange={this.onIDValueChange} placeholder="Enter ID" />
+                    </div>
+                    <div className="form-group col-md-8">
+                        <label htmlFor="exampleFormControlInput1"> Item Name :</label>
+                        <input type="text" className="form-control" id="pwd" value={this.state.itemname} onChange={this.onNameValueChange} placeholder="Enter Name" />
+                    </div>
+                    <div className="form-group col-md-8">
+                        <label htmlFor="exampleFormControlInput1"> Description:</label>
+                        <input type="text" className="form-control" value={this.state.description} onChange={this.onDescriptionValueChange} placeholder="Add Description" />
+                    </div>
+                    <div className="form-group col-md-8">
+                        <label htmlFor="exampleFormControlInput1">Image:</label>
+                        <FileBase64
+                            multiple={false}
+                            onDone={this.getFiles.bind(this)}
+                        />
+                    </div>
+                    <div className="form-group col-md-8">
+                        <label htmlFor="exampleFormControlInput1"> Available Quantity :</label>
+                        <input type="Number" className="form-control" value={this.state.qty} onChange={this.onQtyValueChange} id="email" placeholder="Enter Available Quantity" />
+                    </div>
+                    <div className="form-group col-md-8">
+                        <label htmlFor="exampleFormControlInput1"> Price:</label>
+                        <input type="Number" className="form-control" id="pwd" value={this.state.price} onChange={this.onPriceValueChange} placeholder="Enter Price" />
                     </div>
                     <div className="form-group">
-                        <label className="control-label col-sm-2" >Item Name :</label>
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" id="pwd" value={this.state.itemname} onChange={this.onNameValueChange} placeholder="Enter Name" />
+                        <div className="col-sm-8">
+                            <button className="btn btn-primary col-sm-2" id="subButton" onClick={this.handleChange}>Save</button>
                         </div>
                     </div>
-                    <div className="form-group">
-                        <label className="control-label col-sm-2">Available Quantity :</label>
-                        <div className="col-sm-10">
-                            <input type="Number" className="form-control" value={this.state.qty} onChange={this.onQtyValueChange} id="email" placeholder="Enter Available Quantity" />
-                        </div>
-                    </div>
-                    <div className="form-group">
-                        <label className="control-label col-sm-2" >Price:</label>
-                        <div className="col-sm-10">
-                            <input type="Number" className="form-control" id="pwd" value={this.state.price} onChange={this.onPriceValueChange} placeholder="Enter Price" />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <label className="control-label col-sm-2" >Description:</label>
-                        <div className="col-sm-10">
-                            <input type="text" className="form-control" value={this.state.description} onChange={this.onDescriptionValueChange} placeholder="Add Description" />
-                        </div>
-                    </div>
-
-                    <div className="form-group">
-                        <label className="control-label col-sm-2" >Image:</label>
-                        <div className="col-sm-10">
-                            <FileBase64
-                                multiple={false}
-                                onDone={this.getFiles.bind(this)}
-                               />
-
-                        </div>
-                    </div>
-
-                  
-
-
-
-
                 </form>
-                <button className="btn btn-primary col-sm-2" id="subButton" onClick={this.handleChange}>Save</button>
-
-
-
-
-
-
-
-
-
-
-
-
+            </div>
+        )
+    }
+    render() {
+        return (
+            <div className="container-fluid">
+                <h2 className="custitle">New Item</h2>
+                <div className="col-sm-8 contain">
+                    <hr />
+                    <div className="middle">
+                        {this.form()}
+                    </div>
+                    <div className="form">
+                        <div className="col-sm-8">
+                        </div>
+                    </div>
+                </div>
+                <Links />
             </div>
         );
     }
